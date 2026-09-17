544f8e215ea8af76f036d0682b5afa7e09d31693
MakamSriyasaswini <makamsriyasaswini@gmail.com>
2026-09-17 09:05:36 +0530

    Generate student academic report in report.txt

---
+++ app.py
@@ -0,0 +1,6 @@
+with open("report.txt", "w") as f:
+    f.write("Student Academic Report\n")
+    f.write("Total Users: 120\n")
+    f.write("Active Submissions: 45\n")
+
+print("Report generated.")
