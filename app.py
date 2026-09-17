44d4da577b8a8b8be31fa78ab302fa1e9eca5850
MakamSriyasaswini <makamsriyasaswini@gmail.com>
2026-09-17 09:27:46 +0530

    Update user and submission counts in report

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 with open("report.txt", "w") as f:
     f.write("Student Academic Report\n")
-    f.write("Total Users: 120\n")
-    f.write("Active Submissions: 45\n")
+    f.write("Total Users: 200\n")
+    f.write("Active Submissions: 80\n")
 
 print("Report generated.")
